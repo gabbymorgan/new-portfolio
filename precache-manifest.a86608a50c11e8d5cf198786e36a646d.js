14dc31b8d71cec47de7ed837e2dad5390e95a667
Gabby Pelton <gabriellapelton@gmail.com>
2021-10-19 14:23:48 -0500

    Updates

---
+++ precache-manifest.a86608a50c11e8d5cf198786e36a646d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e23667cfee151d3f969dd6e7a8aed631",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cba0924fdcd09e89a222",
+    "url": "/static/css/2.af400016.chunk.css"
+  },
+  {
+    "revision": "d7b092bccbb2f4c5254d",
+    "url": "/static/css/main.fca40ed2.chunk.css"
+  },
+  {
+    "revision": "cba0924fdcd09e89a222",
+    "url": "/static/js/2.1f1433d0.chunk.js"
+  },
+  {
+    "revision": "ac5a46b438937065c8c5c019709e0fee",
+    "url": "/static/js/2.1f1433d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d7b092bccbb2f4c5254d",
+    "url": "/static/js/main.55c317e0.chunk.js"
+  },
+  {
+    "revision": "2f5ab611fd4164b0120f",
+    "url": "/static/js/runtime-main.63df13e8.js"
+  }
+]);
