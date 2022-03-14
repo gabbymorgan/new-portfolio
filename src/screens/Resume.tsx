cf524db60f50c085654b406446f1c0f1367858bc
Gabriella Morgan <gabriellapelton@gmail.com>
2022-03-14 10:49:45 -0500

    Update resume

--- src/screens/Resume.tsx
+++ src/screens/Resume.tsx
@@ -26,7 +26,7 @@ export class Resume extends Component<Props, State> {
               <img
                 style={{ maxWidth: "100%" }}
                 alt="The updates resume of Gabriella Rose Morgan. For a screen-reader compatible version, please click through."
-                src="https://res.cloudinary.com/dwa3bztx6/image/upload/v1634158372/Portfolio/Gabriella_Morgan_Resume_2021.png"
+                src="https://res.cloudinary.com/dwa3bztx6/image/upload/v1634158372/Portfolio/Gabriella_Morgan_Resume_2022.png"
               />
             )}
           </Col>
